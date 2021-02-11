13f038776db102f6f889e9eff2a138a9c440fb66
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2021-02-11 21:54:24 +0700

    add: handle if not image upload

--- middleware/validationImage.js
+++ middleware/validationImage.js
@@ -2,26 +2,32 @@ const fs = require('fs')
 
 module.exports.validateImage = async (req, res, next) => {
     try {
-        let image = req.file
-        let imagePath = image.path
 
+        let image = req.file
+
+        if (!image) {
+            return res.status(400).json({
+                status: false,
+                error: 'No Image upload'
+            })
+        }
 
         // CHECK IMAGE FILE VALIDATE 
         if (!(image.mimetype).includes('jpeg') && !(image.mimetype).includes('png')
             && !(image.mimetype).includes('jpg')) {
-            await fs.unlinkSync(imagePath)
+            await fs.unlinkSync(image.path)
             return res.json({
                 status: false,
-                msg: 'failed. Allowed Image format jpeg, png, jpg'
+                error: 'failed. Allowed Image format jpeg, png, jpg'
             })
         }
 
         // CHECK IMAGE SIZE
         if (image.size > 1024 * 1024) {
-            await fs.unlinkSync(imagePath)
+            await fs.unlinkSync(image.path)
             return res.json({
                 status: false,
-                msg: 'file too large'
+                error: 'file too large. Max 1 MB'
             })
         }
 
@@ -30,7 +36,7 @@ module.exports.validateImage = async (req, res, next) => {
         console.log(error)
         return res.json({
             status: false,
-            error: 'Validate Error. Please Contact Developers'
+            error: 'Image Validate Error. Please Contact Developers'
         })
     }
 }
